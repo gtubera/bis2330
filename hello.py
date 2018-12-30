844ee4860e68f0a043f0a1651acfcb8cebc15f91
George Tubera <tubera@uiwtx.edu>
2018-12-29 19:56:24 -0600

    Update hello.py
    Updated with comments

--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
+# Adding comments to your code
+# Our first program with comments that allow us to describe code
+# Programs submitted for class will be required to have comemnts
 print("Hello World");
